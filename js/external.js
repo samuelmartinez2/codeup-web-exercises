24a9a1139e466e17b173e00d57536c683aa7e0e8
sam's macbook <samuel2.martinez2@gmail.com>
2022-04-13 16:57:29 -0500

    first html exxercise

---
+++ js/external.js
@@ -0,0 +1,31 @@
+"use strict"
+
+let color = prompt("what is your favorite color");
+console.log(color);
+alert ("Great, " + color + " is my favorite color also");
+
+let price = 3;
+let mermaid = "the little mermaid";
+let brotherbear = " brother bear";
+let hercules = " hercules";
+
+let number = prompt("how many movies did you rent");
+console.log(number);
+confirm("you rented " + number + " movies")
+
+let mermaiddays = prompt("how many days did you rent " + mermaid + " for");
+console.log(mermaiddays);
+confirm("you rented " + mermaid + " " + mermaiddays + " days")
+alert("you rented " + mermaid + " " + mermaiddays + " days")
+
+let brotherbeardays = prompt("how many days did you rent " + brotherbear + " for");
+console.log(brotherbeardays);
+confirm("you rented " + brotherbear + " " +  brotherbeardays + " days")
+alert("you rented " + brotherbear + " " + brotherbeardays + " days")
+
+let herculesdays = prompt("how many days did you rent " + hercules + " for");
+console.log(herculesdays)
+confirm("you rented " + hercules + " " +  herculesdays + " days")
+alert("you rented " + hercules + " " + herculesdays + " days")
+
+
