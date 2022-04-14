2ce00e5302f98708d92c62e89a463a617653e8d8
sam's macbook <samuel2.martinez2@gmail.com>
2022-04-14 09:00:51 -0500

    updated second problem

--- js/external.js
+++ js/external.js
@@ -1,31 +1,47 @@
 "use strict"
 
-let color = prompt("what is your favorite color");
-console.log(color);
-alert ("Great, " + color + " is my favorite color also");
+//  let color = prompt("what is your favorite color");
+//  console.log(color);
+//  alert ("Great, " + color + " is my favorite color also");
+//
+// let price = 3;
+// let mermaid = "the little mermaid";
+// let brotherbear = " brother bear";
+// let hercules = " hercules";
+//
+// let number = parseFloat(prompt("how many movies did you rent"));
+// console.log(number);
+// confirm("you rented " + number + " movies")
+//
+// let mermaiddays = parseFloat(prompt("how many days did you rent " + mermaid + " for"));
+// console.log(mermaiddays);
+// confirm("you rented " + mermaid + " " + mermaiddays + " days")
+//
+//
+// let brotherbeardays = parseFloat(prompt("how many days did you rent " + brotherbear + " for"));
+// console.log(brotherbeardays);
+// confirm("you rented " + brotherbear + " " +  brotherbeardays + " days")
+//
+//
+// let herculesdays = parseFloat(prompt("how many days did you rent " + hercules + " for"));
+// console.log(herculesdays);
+// confirm("you rented " + hercules + " " +  herculesdays + " days")
+//
+//
+// alert((mermaiddays + brotherbeardays + herculesdays) * price );
 
-let price = 3;
-let mermaid = "the little mermaid";
-let brotherbear = " brother bear";
-let hercules = " hercules";
+let google = 400
+let amazon = 380
+let facebook = 350
 
-let number = prompt("how many movies did you rent");
-console.log(number);
-confirm("you rented " + number + " movies")
+let googledays = prompt("how many hours did you work at google?");
+console.log(googledays);
+confirm ("you entered " + googledays+ " days");
 
-let mermaiddays = prompt("how many days did you rent " + mermaid + " for");
-console.log(mermaiddays);
-confirm("you rented " + mermaid + " " + mermaiddays + " days")
-alert("you rented " + mermaid + " " + mermaiddays + " days")
+let amazondays = prompt( "how many days did you work at amazon?");
+console.log(amazondays);
+confirm ("you entered " + amazondays + " days");
 
-let brotherbeardays = prompt("how many days did you rent " + brotherbear + " for");
-console.log(brotherbeardays);
-confirm("you rented " + brotherbear + " " +  brotherbeardays + " days")
-alert("you rented " + brotherbear + " " + brotherbeardays + " days")
-
-let herculesdays = prompt("how many days did you rent " + hercules + " for");
-console.log(herculesdays)
-confirm("you rented " + hercules + " " +  herculesdays + " days")
-alert("you rented " + hercules + " " + herculesdays + " days")
-
-
+let facebookdays = prompt( "how many days did you work at facebook?")
+console.log(facebookdays);
+confirm ("you entered " + facebookdays + " days");
