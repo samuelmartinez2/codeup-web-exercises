5496db4d208413191d2b250bb7debf9105ccf80b
sam's macbook <samuel2.martinez2@gmail.com>
2022-04-15 23:32:58 -0500

    updated external.js exer

--- js/external.js
+++ js/external.js
@@ -30,18 +30,42 @@
 //
 // alert((mermaiddays + brotherbeardays + herculesdays) * price );
 
-let google = 400
-let amazon = 380
-let facebook = 350
-
+//et google = 400
+//let amazon = 380
+//let facebook = 350
+//
 let googledays = prompt("how many hours did you work at google?");
-console.log(googledays);
-confirm ("you entered " + googledays+ " days");
-
+// console.log(googledays);
+// confirm ("you entered " + googledays+ " days");
+//
 let amazondays = prompt( "how many days did you work at amazon?");
-console.log(amazondays);
-confirm ("you entered " + amazondays + " days");
+//console.log(amazondays);
+// confirm ("you entered " + amazondays + " days");
 
 let facebookdays = prompt( "how many days did you work at facebook?")
-console.log(facebookdays);
-confirm ("you entered " + facebookdays + " days");
+// console.log(facebookdays);
+// confirm ("you entered " + facebookdays + " days");
+//
+
+//alert ("your payment for this will be " + facebookdays*prompt( "what is facebooks hourly pay")+" dollars")
+
+//alert ("your will get "+ googledays*prompt("what is googles hourly pay")+ " dollars for this");
+
+//alert ("your pay will be "+ amazondays*prompt("what is amazon's hourly rate")+ " dollars for this");
+
+
+//let total=("your total payment will be" + (facebookdays*prompt( "what is facebooks hourly pay"))+ (googledays*prompt("what is googles hourly pay"))+ (amazondays*prompt("what is amazon's hourly rate")) + "total")
+
+
+let total= facebookdays*prompt( "what is facebooks hourly pay") + googledays*prompt("what is googles hourly pay") + amazondays*prompt("what is amazon's hourly rate")
+
+alert ("your Total will be" + total + "dollars")
+
+console.log (total)
+
+
+
+
+
+
+
