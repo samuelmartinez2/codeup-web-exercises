8e50078b12270918c2dcb232d4eae8977f7b0f86
sam's macbook <samuel2.martinez2@gmail.com>
2022-04-22 09:21:03 -0500

    fizbuz exercise

--- js/warmups.js
+++ js/warmups.js
@@ -1,2 +1,42 @@
 "use strict"
 console.log("Yay I completed my warm up!")
+
+function fizBuz(){
+    for(var i = 1; i <= 20; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0) ) {
+            console.log("Fizz Buzz");
+        }
+        else if (i % 3 == 0) {
+            console.log("Fizz")
+        }
+        else if(i % 5 == 0) {
+            console.log("Buzz")
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+fizBuz()
+
+
+//
+// function fizBuz(){
+//     for(var i = 1; i <= 20; i++) {
+//
+//         if (i % 3 == 0) {
+//             console.log("Fizz")
+//         }
+//         else if(i % 5 == 0) {
+//             console.log("Buzz")
+//         }
+//         else if ((i % 3 == 0) && (i & 5 == 0)) {
+//             console.log("FizzBuzz")
+//         }
+//         else{
+//                 console.log(i)
+//             }
+//         }
+//     }
+//
+// fizBuz()
