44bb8ab16c52e099455d5d66b3dc840c8425468a
sam's macbook <samuel2.martinez2@gmail.com>
2022-04-22 00:16:18 -0500

    break and continue

---
+++ js/break_and_continue.js
@@ -0,0 +1,27 @@
+"use strict";
+(function() {
+let Num = prompt("pick an odd number from 1-50 ")
+function pickNumber(Nu)
+    {
+    for (let Nu = 1; Nu<10; Nu++){
+        if (Nu == Num){ alert("we wont do that number")
+            continue;
+        }
+        console.log(Nu)
+    }
+}
+
+pickNumber()
+
+
+
+
+
+
+
+
+
+
+
+
+}())
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,65 +1,67 @@
 "use strict";
 (function() {
-
-function showMultiplicationTable(num){
-    for (let i= 1; i < 11; i++) {
-        console.log (num+" X "+i+" = "+(i*num))
-    }
-}
-    showMultiplicationTable(7)
-
-let randomN=Math.floor(Math.random()*201)+20
-
-function showRandomNumbers(randomN){
-    for (let i= 0; i < 10; i++) {
-        randomN=Math.floor(Math.random()*201)+20;
-        if (randomN % 2 === 0) {
-            console.log(randomN + "even");
-        } else{
-        console.log(randomN + " odd");
-    }
-}}
-
-showRandomNumbers()
-
-
-    function showmeString(input)
-    {
-        for (let i = 1; i <= 10; i++)
-        {
-            for(let j = 1; j <= i; j++)
-            {
-                console.log(i);
-            }
-        }
-    }
-
-
-showmeString()
-
-function minusFive(input) {
-    for (let i = 100; i >=5; i-=5){
-        console.log(i)
-
-    }
-
-}
-
-minusFive()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
+// function showMultiplicationTable(num){
+//     for (let i= 1; i < 11; i++) {
+//         console.log (num+" X "+i+" = "+(i*num))
+//     }
+// }
+//     showMultiplicationTable(7)
+//
+// let randomN=Math.floor(Math.random()*201)+20
+//
+// function showRandomNumbers(randomN){
+//     for (let i= 0; i < 10; i++) {
+//         randomN=Math.floor(Math.random()*201)+20;
+//         if (randomN % 2 === 0) {
+//             console.log(randomN + "even");
+//         } else{
+//         console.log(randomN + " odd");
+//     }
+// }}
+//
+// showRandomNumbers()
+//
+//
+//     function showmeString()
+//     {
+//         for (let i = 1; i <= 10; i++)
+//         {
+//             for(let j = 1; j <= i; j++)
+//             {
+//                 console.log(i);
+//             }
+//         }
+//     }
+//
+//
+// showmeString()
+//
+// function minusFive() {
+//     for (let i = 100; i >=5; i-=5){
+//         console.log(i)
+//
+//     }
+//
+// }
+//
+// minusFive()
+//
+// //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 }())
