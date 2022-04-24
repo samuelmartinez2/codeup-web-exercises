77a89f1360365845f30a4338d8f4cd1bc4e8804e
sam's macbook <samuel2.martinez2@gmail.com>
2022-04-24 17:34:31 -0500

    study group notes

---
+++ js/study.js
@@ -0,0 +1,38 @@
+"use strict";
+(function(){
+
+    console.log( "hello")
+
+// function add(input,input2) {
+//
+//         return input + input2
+// }
+//
+//     console.log(add(2, 3));
+
+    // function returnRemainder(input1,input2){
+    //     return input1%input2;
+    // }
+    //
+    // console.log(returnRemainder(10, 4));
+//
+//     function addself(addone, addtimes){
+//         let sum =0;
+//     for (let i = 0; i < addtimes; i++){
+//         sum=sum + addone;
+//     }
+//     return sum}
+//
+// console.log (addself(4,6))
+//
+
+    let myName = "sam"
+
+    function translate(nombre){
+        return "hello " + nombre;
+    }
+
+    console.log(translate("bob"));
+
+
+})()
---
+++ js/while.js
@@ -0,0 +1,41 @@
+"use strict";
+(function() {
+// let i =1
+// while ( i <= 65536) {
+//     console.log (i); i*=2
+// }
+
+
+    let randomNumb50to100 = Math.floor(Math.random()*51)+50
+    let custumerbuy=Math.floor(Math.random()*5)+1;
+    let custumer = 0
+
+    do {
+        custumer++;
+        custumerbuy = Math.floor(Math.random() * 5) + 1;
+        //console.log("numbers are: " + randomNumb50to100 + ", " + custumerbuy);
+        if (custumerbuy > randomNumb50to100)
+        {
+            console.log("dont have that many");
+        } else {
+            randomNumb50to100 -= custumerbuy;
+            console.log("customer ordered "+ custumerbuy + ".  we now have " + randomNumb50to100 + " left")
+        }}
+    while (0 < randomNumb50to100);{
+        console.log( "")
+    }
+
+
+
+    // custumerbuy;
+    //  console.log(custumerbuy);
+    //  console.log (randomNumb50to100);
+    //      break;
+    // } while (custumerbuy <= randomNumb50to100) {console.log ("customer bought "+ custumerbuy + " we have " + (randomNumb50to100 - custumerbuy) + " left")}
+    //
+    //
+
+
+
+
+}())
