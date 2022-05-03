5aaa6e3215314b512ea8811695493ebf6d6ffa67
sam's macbook <samuel2.martinez2@gmail.com>
2022-05-03 14:01:43 -0500

    flexbox still working

--- js/warmup-loops-arrays-objects.js
+++ js/warmup-loops-arrays-objects.js
@@ -43,13 +43,13 @@ function sumOfElements(arr) {
     return bucket;
 }
 //same as above
-function sumOfElements(arr) {
-    let bucket = 0
-    for (let i =0; i < arrOfNums.lenght; i++) {
-        bucket += arrOfNums[i];
-    }
-    return bucket;
-}
+// function sumOfElements(arr) {
+//     let bucket = 0
+//     for (let i =0; i < arrOfNums.lenght; i++) {
+//         bucket += arrOfNums[i];
+//     }
+//     return bucket;
+// }
 
 console.log(sumOfElements([1, 2, 3, 4, 5]));
 console.log(sumOfElements([23, 34, 12, 23]));
@@ -59,3 +59,58 @@ console.log(sumOfElements([23, 34, 12, 23]));
 //     let numberString =  numberArray.join(',')
 //     console.log(numberString)
 // }
+
+//
+// Create a function, returnProductEssentialDetails, that takes in a product object and returns a simpler product object containing only the product name and price in cents.
+//
+//     Examples...
+//
+// let product1 = {
+//     name: 'Hammar',
+//     priceInCents: 400,
+//     description: 'It is a a hammar.',
+//     inventory: 25034
+// }
+
+// couldnt get this at all
+// function returnProductEssentialDetails(item1) {
+//     let name = "coke";
+//     let price = 1.00;
+//     let type = "drink";
+//     return name +;
+// }
+//
+// console.log(returnProductEssentialDetails(item1));
+
+
+
+let product1 = {
+    name: 'Hammar',
+    priceInCents: 400,
+    description: 'It is a a hammar.',
+    inventory: 25034
+}
+
+let product2 = {
+    name: 'Computer',
+    priceInCents: 40000,
+    description: 'It is a computer.',
+    inventory: 33000
+}
+
+let product3 = {
+    name: 'Ruler',
+    priceInCents: 1000,
+    description: 'It is a ruler.',
+    inventory: 2200
+}
+
+
+
+function returnProductEssentialDetails(product) {
+    return {
+        name: product.name,
+        priceInCents: product.priceInCents
+    }
+}
+console.log(returnProductEssentialDetails(product1));
