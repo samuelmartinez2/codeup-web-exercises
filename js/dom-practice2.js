ec52399cd0b2bb747e7bac2c48ea5c34f97954b6
sam's macbook <samuel2.martinez2@gmail.com>
2022-05-16 15:08:30 -0500

    dom exercise complete

---
+++ js/dom-practice2.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//click Event
+
+
+
+
+
+
+// SUBMIT EVENT
+
+document.querySelector("#form").addEventListener("submit",Function(event) {
+    event.preventDefault();
+    let color = document.getElemetById("text-color").value;
+})
+
